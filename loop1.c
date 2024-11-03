9a02850c10089632939dd3f81c9aa0c46f2d7d1c
chandrakiran18 <chandrakiranbobbadi@gmail.com>
2024-11-03 11:24:30 +0530

    Add files via upload

---
+++ loop1.c
@@ -0,0 +1,16 @@
+/*    
+        Q. Write a program to print natural numbers from 10 to 20 when initial loop 
+         counter is initialized to 0.                                                               
+*/
+#include <stdio.h>
+
+int main(){
+    int i=10;
+    while (10<=i&&i<=20)
+    {
+        printf("%d \n",i);
+        i++;
+    }
+    
+    return 0;
+}
